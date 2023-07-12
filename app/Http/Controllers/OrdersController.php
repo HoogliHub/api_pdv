f427d9093b6bbe4eccf9173e70b35aeff0f71405
walissonSSousa <walisson.sousa@innovationlatam.com>
2023-07-12 18:12:34 -0300

    add_new_orders_rule

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class OrdersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+
+            $url = config('enjoy.url');
+            $reply = Http::get($url . 'api/orders');
+
+            $response = $reply->json();
+
+            return $response;
+        } catch (\Exception $e) {
+            throw new \Exception($e->getCode());
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $url = config('enjoy.url');
+
+            $requestClient = $request->all();
+
+
+            if (isset($requestClient['Order']['point_sale'])) {
+
+            }
+
+            if (isset($requestClient['Order']['shipment'])) {
+
+            }
+
+            if (isset($requestClient['Order']['shipment_value'])) {
+
+            }
+
+            if (isset($requestClient['Order']['payment_form'])) {
+                $data['city'] = $requestClient['Customer']['city'];
+            }
+
+            if (isset($requestClient['Order']['Customer'])) {
+
+            }
+
+            if (isset($requestClient['Order']['CustomerAddress'])) {
+
+            }
+
+            if (isset($requestClient['Order']['ProductsSold'])) {
+
+            }
+
+            $response = Http::post($url . 'api/order', $data);
+
+            return $response;
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getCode());
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $url = config('enjoy.url');
+
+            $reply = Http::get($url . 'api/order/' . $id);
+
+            $response = $reply->json();
+
+            return $response;
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getCode());
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        if (isset($requestClient['Order']['point_sale'])) {
+
+        }
+
+        if (isset($requestClient['Order']['shipment'])) {
+
+        }
+
+        if (isset($requestClient['Order']['shipment_value'])) {
+
+        }
+
+        if (isset($requestClient['Order']['payment_form'])) {
+            $data['city'] = $requestClient['Customer']['city'];
+        }
+
+        if (isset($requestClient['Order']['Customer'])) {
+
+        }
+
+        if (isset($requestClient['Order']['CustomerAddress'])) {
+
+        }
+
+        if (isset($requestClient['Order']['ProductsSold'])) {
+
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+            $url = config('enjoy.url');
+
+            $response = Http::delete($url . 'api/order/' . $id);
+
+            return $response;
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getCode());
+        }
+    }
+}
--- routes/api/enjoy.php
+++ routes/api/enjoy.php
@@ -3,17 +3,26 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\ClientsController;
+use App\Http\Controllers\OrdersController;
 
 Route::group(['prefix' => 'enjoy', 'as' => 'enjoy.' ], function () {
-
+//   PRODUTOS
     Route::resource('products', ProductsController::class, ['only' => [
     'index', 'create', 'show', 'store', 'destroy'
     ]]);
     Route::put('/products/{id}', [ProductsController::class, 'edit']);
 
+//  Clientes
     Route::resource('clients', ClientsController::class, ['only' => [
         'index', 'create', 'show', 'store', 'destroy'
     ]]);
 
     Route::put('/clients/{id}', [ClientsController::class, 'edit']);
+
+//  Pedidos
+    Route::resource('orders', OrdersController::class, ['only' => [
+        'index', 'create', 'show', 'store', 'destroy'
+    ]]);
+
+    Route::put('/clients/{id}', [OrdersController::class, 'edit']);
 });
